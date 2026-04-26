2e1b335d5c719d37944eb711572998116a64469a
hariprasadpantrangam <hariprasadp7799@gmail.com>
2026-04-26 13:16:36 +0530

    new features

--- matrimonial.js
+++ matrimonial.js
@@ -1,59 +1,26 @@
 const profiles = [
   {
-    name: "hariprasad",
+    name: "Anjali",
     age: 25,
     profession: "Software Engineer",
     city: "Hyderabad",
+    mobile:"9573572830",
+    education:"bsc",
+    caste:"sc-mala",
+    religion:"hindu",
+    img: "../onlineshopping/public/bigili.jpeg"
+  },
+  {
+    name: "hariprasad",
+    age: 28,
+    profession: "software engineer",
+    city: "Chennai",
+      city: "Hyderabad",
     mobile:"9573572830",
     education:"msc",
     caste:"sc-mala",
     religion:"hindu",
     img: "../onlineshopping/public/hari.jpg"
-  },
-  {
-    name: "bigili",
-    age: 28,
-    profession: "Doctor",
-    city: "Chennai",
-     mobile:"7386618287",
-      education:"msc",
-    caste:"sc-mala",
-    religion:"hindu",
-    img: "../onlineshopping/public/bigili.jpeg"
-  },
-
-    {
-    name: "ramesh",
-    age: 28,
-    profession: "Doctor",
-    city: "Chennai",
-     mobile:"7386618287",
-      education:"ba",
-    caste:"bs-devanga",
-    religion:"hindu",
-    img: "../onlineshopping/public/avengers.jpg"
-  },
-    {
-    name: "bigili",
-    age: 28,
-    profession: "actress",
-    city: "bangalore",
-     mobile:"7386618287",
-      education:"msc",
-    caste:"bc-b",
-    religion:"hindu",
-    img: "../onlineshopping/public/anushka.jpg"
-  },
-    {
-    name: "chiranjeevi konidela",
-    age: 50,
-    profession: "Actor",
-    city: "hyd",
-     mobile:"7386618287",
-      education:"ba",
-    caste:"oc-naidu",
-    religion:"hindu",
-    img: "../onlineshopping/public/chiru.jpg"
   }
 ];
 
@@ -63,7 +30,7 @@ const container = document.getElementById("cardContainer");
 function displayProfiles() {
   container.innerHTML = "";
 
-  profiles.forEach((p) => {
+  profiles.forEach((p, index) => {
     const card = document.createElement("div");
     card.className = "card";
 
@@ -75,10 +42,10 @@ function displayProfiles() {
         <p>${p.profession}</p>
         <p>${p.city}</p>
         <p>${p.mobile}</p>
-          <p>${p.education}</p>
-            <p>${p.caste}</p>
-              <p>${p.religion}</p>
-        // <button class="btn" onclick="deleteProfile(${index})">Remove</button>
+        <p>${p.education}</p>
+        <p>${p.caste}</p>
+        <p>${p.religion}</p>
+        <button class="btn" onclick="deleteProfile(${index})">Remove</button>
       </div>
     `;
 
@@ -86,16 +53,16 @@ function displayProfiles() {
   });
 }
 
-//Add profile
+// Add profile
 function addProfile() {
   const name = document.getElementById("name").value;
   const age = document.getElementById("age").value;
   const profession = document.getElementById("profession").value;
   const city = document.getElementById("city").value;
-   const mobile = document.getElementById("mobile").value;
-    const education = document.getElementById("education").value;
-     const caste = document.getElementById("caste").value;
-      const religion = document.getElementById("religion").value;
+    const mobile = document.getElementById("mobile").value;
+      const education = document.getElementById("education").value;
+        const caste = document.getElementById("caste").value;
+          const religion = document.getElementById("religion").value;
   const img = document.getElementById("img").value;
 
   if (!name || !age) {
@@ -103,11 +70,11 @@ function addProfile() {
     return;
   }
 
-  profiles.push({ name, age, profession, city, mobile,education,caste,religion, img });
+  profiles.push({ name, age, profession, city,mobile,education,caste,religion, img });
   displayProfiles();
 }
 
-//Delete profile
+// Delete profile
 function deleteProfile(index) {
   profiles.splice(index, 1);
   displayProfiles();
